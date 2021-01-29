4ee010b48a6f8f26afe695bec0c9dd10ba4ac3d3
amjadi95 <amjadi.9574@gmail.com>
2021-01-29 17:55:00 +0330

    add database, table, dbcontext

---
+++ netcoreWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using netcoreWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Commodity> Commodities { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> opt):base(opt)
+        {
+
+        }
+    }
+    
+}
